4239b235d80317c7a808479ca5924fa76c66ee64
Eric Jae-Min Joo <ericjaeminjoo@gmail.com>
2020-02-22 18:57:07 -0500

    Add global styling helper component from styled-components library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  // minimal CSS reset
+  *,
+  *:before,
+  *:after {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+  }
+
+  html {
+  box-sizing: border-box;
+  font-size: 62.5%;
+  }
+
+  body {
+  font-size: 1.6rem;
+  }
+`;
 
 class App extends Component {
   render() {
-    return <div>Hello, World...again!</div>;
+    return (
+      <Fragment>
+        <GlobalStyle />
+        <div>Hello, World...again!</div>;
+      </Fragment>
+    );
   }
 }
 
