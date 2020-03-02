3e09fb39f082d41afaac2c062b590c3b3f21ab34
Eric Jae-Min Joo <ericjaeminjoo@gmail.com>
2020-03-02 15:10:28 -0500

    Fix deployment failure issue and update logo/favicon

--- src/components/GitHubReposSection.js
+++ src/components/GitHubReposSection.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import fetchGitHubRepos from '../api/api';
